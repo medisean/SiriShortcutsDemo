3f66fb1601d7a139c157a11672455ed08d94efa1
zouliangming <liangmingzou@163.com>
2018-12-11 14:42:38 +0800

    Init projects

---
+++ SiriShortcutsDemo/SiriShortcutsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcutsDemo
+//
+//  Created by 邹亮明 on 2018/12/11.
+//  Copyright © 2018 zlm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
