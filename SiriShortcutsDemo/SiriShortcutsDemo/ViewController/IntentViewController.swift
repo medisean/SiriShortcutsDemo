4a9aa8316cb11e35e1f3e6a516b0652c8f1ef728
zouliangming <liangmingzou@163.com>
2019-01-03 15:09:08 +0800

    fix: siri bug

--- SiriShortcutsDemo/SiriShortcutsDemo/ViewController/IntentViewController.swift
+++ SiriShortcutsDemo/SiriShortcutsDemo/ViewController/IntentViewController.swift
@@ -11,40 +11,69 @@ import Intents
 import IntentsUI
 
 class IntentViewController: UIViewController {
+    
+    private var isAdded: Bool = false
+    private var voiceShortCut: INVoiceShortcut?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadVoice()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func addButtonPressed(_ sender: Any) {
-        let intent = IntentDemoIntent()
-        if let shortCuts = INShortcut(intent: intent) {
-            let viewController = INUIAddVoiceShortcutViewController(shortcut: shortCuts)
-            viewController.delegate = self
-            present(viewController, animated: true, completion: nil)
+    private func loadVoice() {
+        INVoiceShortcutCenter.shared.getAllVoiceShortcuts { (voiceShortCuts, error) in
+            guard let voiceShortCuts = voiceShortCuts, error == nil else { return }
+            for voiceShortCut in voiceShortCuts {
+                if voiceShortCut.shortcut.intent is IntentDemoIntent {
+                    self.voiceShortCut = voiceShortCut
+                    self.isAdded = true
+                    break
+                }
+            }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        if isAdded {
+            let editVoiceShortcutViewController = INUIEditVoiceShortcutViewController(voiceShortcut: voiceShortCut!)
+            editVoiceShortcutViewController.delegate = self
+            present(editVoiceShortcutViewController, animated: true, completion: nil)
+        } else {
+            let intent = IntentDemoIntent()
+            if let shortCuts = INShortcut(intent: intent) {
+                let viewController = INUIAddVoiceShortcutViewController(shortcut: shortCuts)
+                viewController.delegate = self
+                present(viewController, animated: true, completion: nil)
+            }
+        }
     }
-    */
-
 }
 
 extension IntentViewController: INUIAddVoiceShortcutViewControllerDelegate {
     func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
+        loadVoice()
         controller.dismiss(animated: true, completion: nil)
     }
     
     func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension IntentViewController: INUIEditVoiceShortcutViewControllerDelegate {
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didUpdate voiceShortcut: INVoiceShortcut?, error: Error?) {
+        loadVoice()
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didDeleteVoiceShortcutWithIdentifier deletedVoiceShortcutIdentifier: UUID) {
+        loadVoice()
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func editVoiceShortcutViewControllerDidCancel(_ controller: INUIEditVoiceShortcutViewController) {
+        loadVoice()
         controller.dismiss(animated: true, completion: nil)
     }
 }
